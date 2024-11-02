d19f649a55055c936d5599e4143ced70292339ee
modeco80 <lily.modeco80@protonmail.ch>
2024-11-02 02:52:43 -0400

    add a interface for process launching
    This is to allow external usage of the library to control process launching (and, for one example, place processes we launch into special cgroups.)

---
+++ src/ProcessInterface.ts
@@ -0,0 +1,36 @@
+import {type Stream, EventEmitter, Readable, Writable} from 'node:stream';
+
+export type StdioOption =
+	| 'pipe'
+	| 'overlapped'
+	| 'ipc'
+	| 'ignore'
+	| 'inherit'
+	| Stream
+	| number
+	| undefined;
+
+// subset of options. FIXME: Add more!!!
+export interface ProcessLaunchOptions {
+    stdin?: StdioOption,
+    stdout?: StdioOption,
+    stderr?: StdioOption
+}
+
+export interface IProcess extends EventEmitter {
+    stdin: Writable | null;
+    stdout: Readable | null;
+    stderr: Readable | null;
+
+
+    // Escape hatch; only use this if you have no choice
+    //native() : any;
+
+    kill(signal?: number | NodeJS.Signals): boolean;
+
+}
+
+// Launches a processs.
+export interface IProcessLauncher {
+    launch(command: string, opts?: ProcessLaunchOptions) : IProcess;
+}
